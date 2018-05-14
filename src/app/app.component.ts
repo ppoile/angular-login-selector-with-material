97be25fa2cbe246e9ce8cef29abf856911f7de8b
Andi Meier <andi@daemeier-clan.ch>
2018-05-14 08:33:29 +0200

    app: implement login selection with checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  model = {
+    allLogins: [
+      'London',
+      'London Kraftwerk',
+      'Dover',
+    ],
+    selectedLogins: { },
+  };
+  queryParams = { };
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router) {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      console.log(params);
+      this.queryParams = params;
+      this.parseQueryParams();
+    });
+  }
+
+  parseQueryParams() {
+    if (this.queryParams['hideLogins'] === 'None') {
+      this.selectAll();
+    }
+    else if (this.queryParams['hideLogins'] === 'All') {
+      this.selectNone();
+    }
+  }
+
+  onAll() {
+    this.selectAll();
+    this.queryParams = {...this.queryParams, hideLogins: 'None'};
+    this.router.navigate(['dashboard'], { queryParams: this.queryParams });
+  }
+
+  onNone() {
+    this.selectNone();
+    this.queryParams = {...this.queryParams, hideLogins: 'All'};
+    this.router.navigate(['dashboard'], { queryParams: this.queryParams });
+  }
+
+  selectAll() {
+    for (var login of this.model.allLogins) {
+      this.model.selectedLogins[login] = true;
+    }
+  }
+
+  selectNone() {
+    for (var login of this.model.allLogins) {
+      this.model.selectedLogins[login] = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,27 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     MatButtonModule,
     MatCardModule,
     MatCheckboxModule,
+    MatInputModule,
     MatRadioModule,
   ],
   providers: [],
